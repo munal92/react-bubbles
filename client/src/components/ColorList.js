2e351964c5200009f76ddee60c193efbb51db384
Mucteba Fatih Unal <m_fattih@hotmail.com>
2020-05-01 20:47:19 -0700

    Revert "mvp + stretch"
    This reverts commit 70eb9a244f2ed9a37f2d5052fee5d00e8c13d650.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
-  code: { hex: "" },
+  code: { hex: "" }
 };
 
 const ColorList = ({ colors, updateColors }) => {
+  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
-  const editColor = (color) => {
+  const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
-  console.log("colorEdit", colorToEdit);
-  console.log("colorList", colors);
-  const saveEdit = (e) => {
+
+  const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    axiosWithAuth()
-      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
-      .then((res) => {
-        console.log("editingColor", res.data);
-        updateColors([
-          ...colors.filter((item) => {
-            return item.id !== colorToEdit.id;
-          }),
-          res.data,
-        ]);
-        setEditing(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
   };
 
-  const deleteColor = (color) => {
+  const deleteColor = color => {
     // make a delete request to delete this color
-    axiosWithAuth()
-      .delete(`/api/colors/${color.id}`)
-      .then((res) => {
-        // console.log("deletingColor", res);
-        updateColors(colors.filter((item) => item.id !== color.id));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  // -----------------------addingCOlor
-
-  const submitForm = (e) => {
-    e.preventDefault();
-
-    axiosWithAuth()
-      .post(`/api/colors`, colorToAdd)
-      .then((res) => {
-        // console.log("AddingColor", res);
-        updateColors([...colors, colorToAdd]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map((color) => (
+        {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span
-                className="delete"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  deleteColor(color);
-                }}
-              >
-                x
+              <span className="delete" onClick={e => {
+                    e.stopPropagation();
+                    deleteColor(color)
+                  }
+                }>
+                  x
               </span>{" "}
               {color.color}
             </span>
@@ -100,7 +56,7 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={(e) =>
+              onChange={e =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
@@ -109,10 +65,10 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             hex code:
             <input
-              onChange={(e) =>
+              onChange={e =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value },
+                  code: { hex: e.target.value }
                 })
               }
               value={colorToEdit.code.hex}
@@ -126,29 +82,6 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
-      <form onSubmit={submitForm}>
-        <label>
-          ColorName
-          <input
-            name="color"
-            value={colorToAdd.color}
-            onChange={(e) => {
-              setColorToAdd({ ...colorToAdd, color: e.target.value });
-            }}
-          />
-        </label>
-        <label>
-          ColorCOde
-          <input
-            name="code.hex"
-            value={colorToAdd.code.hex}
-            onChange={(e) => {
-              setColorToAdd({ ...colorToAdd, code: { hex: e.target.value } });
-            }}
-          />
-        </label>
-        <button type="submit">AddColor</button>
-      </form>
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,56 +1,58 @@
 import React, { useState } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { Link } from "react-router-dom";
+import {axiosWithAuth} from "../utils/axiosWithAuth"
+import {Link} from "react-router-dom";
+
 
 const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [userInfo, setUserInfo] = useState({
-    username: "",
-    password: "",
-  });
+  const [userInfo,setUserInfo] = useState({
+    username:"",
+    password:""
+  })
 
-  const handleChange = (e) => {
-    e.persist();
-    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
-  };
-  console.log("Form Info", userInfo);
-  const submitForm = (e) => {
-    e.preventDefault();
+const handleChange = (e) => {
+  e.persist();
+  setUserInfo({...userInfo,[e.target.name]: e.target.value });
 
-    axiosWithAuth()
-      .post("/api/login", userInfo)
-      .then((res) => {
-        // console.log(res)
-        window.localStorage.setItem("token", res.data.payload);
-        props.history.push("/bubblespage");
-      })
-      .catch((err) => console.log(err));
-  };
+}
+console.log("Form Info", userInfo)
+const submitForm = (e) => {
+  e.preventDefault();
+
+  axiosWithAuth()
+  .post('/api/login',userInfo)
+  .then(res => {
+    // console.log(res)
+ window.localStorage.setItem('token',res.data.payload)
+ props.history.push("/bubblespage")
+  })
+  .catch(err => console.log(err))
+  
+}
+
 
   return (
     <div className="login_cont">
-      <h1>
-        Welcome to the Bubble App! <Link to="/bubblespage">&#955;</Link>
-      </h1>
+      <h1>Welcome to the Bubble App! <Link to="/bubblespage">TryPrivateR</Link></h1>
       <form onSubmit={submitForm}>
         <label>
           Username
           <input
-            name="username"
-            value={userInfo.username}
-            onChange={handleChange}
+          name="username"
+          value={userInfo.username}
+          onChange={handleChange}
           />
         </label>
         <label>
           Password
           <input
-            name="password"
-            value={userInfo.password}
-            onChange={handleChange}
+          name="password"
+          value={userInfo.password}
+          onChange={handleChange}
           />
         </label>
-        <button type="submit">Log In</button>
+        <button type="submit" >Log In</button>
       </form>
     </div>
   );
