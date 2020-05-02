a1a583ef781158ed93dc9d26b09b0d985b72710e
unknown <m_fattih@hotmail.com>
2020-05-01 19:07:23 -0700

    post + get done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BubblePage from "./components/BubblePage"
+import PrivateRoute from "./components/PrivateRoute"
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -8,12 +9,16 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Switch>
+        <PrivateRoute path="/bubblespage" component={BubblePage}/>  
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+       </Switch>
       </div>
+     
     </Router>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth"
+import {Link} from "react-router-dom";
 
-const Login = () => {
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [userInfo,setUserInfo] = useState({
+    username:"",
+    password:""
+  })
+
+const handleChange = (e) => {
+  e.persist();
+  setUserInfo({...userInfo,[e.target.name]: e.target.value });
+
+}
+console.log("Form Info", userInfo)
+const submitForm = (e) => {
+  e.preventDefault();
+
+  axiosWithAuth()
+  .post('/api/login',userInfo)
+  .then(res => {
+    // console.log(res)
+ window.localStorage.setItem('token',res.data.payload)
+ props.history.push("/bubblespage")
+  })
+  .catch(err => console.log(err))
+  
+}
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="login_cont">
+      <h1>Welcome to the Bubble App! <Link to="/bubblespage">TryPrivateR</Link></h1>
+      <form onSubmit={submitForm}>
+        <label>
+          Username
+          <input
+          name="username"
+          value={userInfo.username}
+          onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+          name="password"
+          value={userInfo.password}
+          onChange={handleChange}
+          />
+        </label>
+        <button type="submit" >Log In</button>
+      </form>
+    </div>
   );
 };
 
